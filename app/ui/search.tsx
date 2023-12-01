c810867466338ec37c6ae98acda579d44c9dd95b
myselfcarvajal <juandiegomartinez@unicesar.edu.co>
2023-12-01 18:19:56 -0500

    Adding pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { pages } from 'next/dist/build/templates/app-page';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import {useDebouncedCallback} from 'use-debounce'
 
@@ -17,6 +18,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query');
     }
+    params.set("page","1")
     replace(`${pathname}?${params.toString()}`);
   }, 300);
 
